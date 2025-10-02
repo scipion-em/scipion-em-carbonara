d42d1bce45ca79f225630adfc379d2d7ad514545
Marta Martinez <mmmtnez@cnb.csic.es>
2025-10-02 13:40:34 +0200

    added CARBonAra method citation

---
+++ carbonara/bibtex.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# **************************************************************************
+# *
+# * Authors:     Marta Martinez (mmmtnez@cnb.csic.es)
+# *              Roberto Marabini (roberto@cnb.csic.es)
+# *
+# * National Center for Biotechnology (CNB-CSIC)
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+"""
+@ARTICLE{Krapp2024-dw,
+  title     = "Context-aware geometric deep learning for protein sequence
+               design",
+  author    = "Krapp, Lucien F and Meireles, Fernando A and Abriata, Luciano A
+               and Devillard, Jean and Vacle, Sarah and Marcaida, Maria J and
+               Dal Peraro, Matteo",
+  abstract  = "Protein design and engineering are evolving at an unprecedented
+               pace leveraging the advances in deep learning. Current models
+               nonetheless cannot natively consider non-protein entities within
+               the design process. Here, we introduce a deep learning approach
+               based solely on a geometric transformer of atomic coordinates
+               and element names that predicts protein sequences from backbone
+               scaffolds aware of the restraints imposed by diverse molecular
+               environments. To validate the method, we show that it can
+               produce highly thermostable, catalytically active enzymes with
+               high success rates. This concept is anticipated to improve the
+               versatility of protein design pipelines for crafting desired
+               functions.",
+  journal   = "Nat. Commun.",
+  publisher = "Springer Science and Business Media LLC",
+  volume    =  15,
+  number    =  1,
+  pages     = "6273",
+  month     =  jul,
+  year      =  2024,
+  copyright = "https://creativecommons.org/licenses/by-nc-nd/4.0",
+  language  = "en"
+}
+"""
