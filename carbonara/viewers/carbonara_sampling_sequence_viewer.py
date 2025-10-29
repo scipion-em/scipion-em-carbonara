1f78f9859bf30fe00c214dfda372aaa2f5f10e46
Marta Martinez <mmmtnez@cnb.csic.es>
2025-10-29 22:22:43 +0100

    Modification of design and replacement of Viewer by ProtocolViewer to make it functional

--- carbonara/viewers/carbonara_sampling_sequence_viewer.py
+++ carbonara/viewers/carbonara_sampling_sequence_viewer.py
@@ -28,65 +28,74 @@
 import os
 import pyworkflow.protocol.params as params
 from ..protocols import CarbonaraSamplingSequence
-from pyworkflow.viewer import DESKTOP_TKINTER, Viewer, WEB_DJANGO
+from pyworkflow.viewer import DESKTOP_TKINTER, WEB_DJANGO, ProtocolViewer
 from tkinter.messagebox import showerror
 from pyworkflow.gui.text import openTextFileEditor
 
-SEQANDSCORE_FILE = 0
-FASTA_FILE= 1
-CLUSTAL_FILE = 2
-CLUSTALSUMM_FILE = 3
-
-class CarbonaraViewer(Viewer):
+class CarbonaraViewer(ProtocolViewer):
     """ Visualize the output of protocol carbonara sampling sequence """
     _label= 'carbonara viewer'
     _environments = [DESKTOP_TKINTER, WEB_DJANGO]
     _targets = [CarbonaraSamplingSequence]
 
     def __init__(self, **kwargs):
-        Viewer.__init__(self, **kwargs)
+        ProtocolViewer.__init__(self, **kwargs)
 
     def _defineParams(self, form):
-        form.addSection(label='Visualization of CARBonAra result summary text files')
-        form.addParam('displayFiles', params.EnumParam,
-                      choices=['Sequences with scores file', 'Sequences in fasta format', 
-                               "Alignment with Clustal Omega", "Summary of the Clustal Alignment"],
-                      default=SEQANDSCORE_FILE,
-                      display=params.EnumParam.DISPLAY_HLIST,
-                      label='Select the file to visualize',
-                      help='')
+        form.addSection(label='Visualization of CARBonAra results')
+        group = form.addGroup('Scores')
+        group.addParam('displayScores', params.LabelParam,
+                       label='CARBonAra sequence scores', 
+                       help='List of ID sequences and CARBonAra scores')
+        group = form.addGroup('Sequences')
+        group.addParam('displaySequences', params.LabelParam,
+                       label='CARBonAra sampled sequences', 
+                       help='Sampled sequences in .fasta format. The starting sequence' \
+                       ' is also included.')
+        group = form.addGroup('Alignment')
+        group.addParam('displayAlignment', params.LabelParam,
+                       label='ClustalOmega alignment', 
+                       help='Sampled sequences in .aln format. The starting sequence' \
+                       ' is also included.')
+        group = form.addGroup('Alignment Summary')
+        group.addParam('displayAlignmentSummary', params.LabelParam,
+                       label='Clustal Omega alignment summary', 
+                       help='Starting sequence and ClustalOmega identity and similarity' \
+                       ' symbols.')
+
 
     def _getVisualizeDict(self):
         return {
-            'displayFiles': self._showTextFiles,
+            'displayScores': self._showScores,
+            'displaySequences': self._showSequences,
+            'displayAlignment': self._showAlignment,
+            'displayAlignmentSummary': self._showAlignmentSummary
         }
     
-    def _showTextFiles(self, obj, **args):
+    # view all text files located in protocol extra folder
+
+    def _showScores(self, obj, **args):
         self.dir_path = os.path.abspath(self.protocol._getExtraPath())
-        subdirectory = "carbonara_results"
-        self.subdir_path = os.path.join(self.dir_path, subdirectory)
-        
-        # view all text files located in protocol extra folder
+        # subdirectory = "carbonara_results"
+        # self.subdir_path = os.path.join(self.dir_path, subdirectory)
+        seqAndScoreFile = os.path.join(self.dir_path, "sorted_scores.txt")
+        if os.path.exists(seqAndScoreFile ):
+            openTextFileEditor(seqAndScoreFile)
 
-        if self.displayFiles == SEQANDSCORE_FILE:
-            seqAndScoreFile = os.path.join(self.dir_path, "sorted_scores.txt")
-            if os.path.exists(seqAndScoreFile ):
-                openTextFileEditor(seqAndScoreFile)
+    def _showSequences(self, obj, **args):
+        fastaFile = os.path.join(self.dir_path, "merged.fasta")
+        if os.path.exists(fastaFile):
+            openTextFileEditor(fastaFile)
 
-        if self.displayFiles == FASTA_FILE:
-            fastaFile = os.path.join(self.dir_path, "merged.fasta")
-            if os.path.exists(fastaFile):
-                openTextFileEditor(fastaFile)
+    def _showAlignment(self, obj, **args):      
+        clustalAlignFile = os.path.join(self.dir_path, "clustal.aln")
+        if os.path.exists(clustalAlignFile):
+            openTextFileEditor(clustalAlignFile)
 
-        if self.displayFiles == CLUSTAL_FILE:
-            clustalAlignFile = os.path.join(self.dir_path, "clustal.aln")
-            if os.path.exists(clustalAlignFile):
-                openTextFileEditor(clustalAlignFile)
-
-        if self.displayFiles == CLUSTALSUMM_FILE:
-            clustalAlignFileSummaryFile = os.path.join(self.dir_path, "clustal_summary.aln")
-            if os.path.exists(clustalAlignFileSummaryFile):
-                openTextFileEditor(clustalAlignFileSummaryFile)
+    def _showAlignmentSummary(self, obj, **args):  
+        clustalAlignFileSummaryFile = os.path.join(self.dir_path, "clustal_summary.aln")
+        if os.path.exists(clustalAlignFileSummaryFile):
+            openTextFileEditor(clustalAlignFileSummaryFile)
     
     def errorWindow(tkParent, msg):
         try:
